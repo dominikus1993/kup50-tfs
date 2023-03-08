940566f6234b40c7055aa68111e610a74011d481
Dominik.Kotecki <dominikus1910@gmail.com>
2023-03-08 18:31:25 +0100

    add GetCommits

--- internal/git/client.go
+++ internal/git/client.go
@@ -2,6 +2,7 @@ package git
 
 import (
 	"context"
+	"errors"
 
 	"github.com/microsoft/azure-devops-go-api/azuredevops"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/git"
@@ -10,14 +11,33 @@ import (
 type AzureDevopsClient struct {
 	organizationUrl string
 	token           string
-	gitClient       *git.Client
+	project         string
+	gitClient       git.Client
 }
 
-func NewAzureDevopsClient(ctx context.Context, organizationUrl, token string) (*AzureDevopsClient, error) {
+func NewAzureDevopsClient(ctx context.Context, organizationUrl, token, project string) (*AzureDevopsClient, error) {
 	connection := azuredevops.NewPatConnection(organizationUrl, token)
 	gitClient, err := git.NewClient(ctx, connection)
 	if err != nil {
 		return nil, err
 	}
-	return &AzureDevopsClient{organizationUrl: organizationUrl, token: token, gitClient: &gitClient}, nil
+	return &AzureDevopsClient{organizationUrl: organizationUrl, token: token, gitClient: gitClient}, nil
+}
+
+func (client *AzureDevopsClient) GetCommits(ctx context.Context, author string) error {
+	repositories, err := client.gitClient.GetRepositories(ctx, git.GetRepositoriesArgs{Project: &client.project})
+	if err != nil {
+		return err
+	}
+	for _, repo := range *repositories {
+		repoId := repo.Id.String()
+		commits, commitErr := client.gitClient.GetCommits(ctx, git.GetCommitsArgs{RepositoryId: &repoId, Project: &client.project, SearchCriteria: &git.GitQueryCommitsCriteria{Author: author}})
+		if err != nil {
+			err = errors.Join(err, commitErr)
+			continue
+		}
+		for _, commit := range *commits {
+
+		}
+	}
 }
