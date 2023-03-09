2561494dd4b70b6d2423c6b453c2fa8561dcb5e4
Dominik.Kotecki <dominikus1910@gmail.com>
2023-03-09 18:22:59 +0100

    .

--- go.mod
+++ go.mod
@@ -13,7 +13,9 @@ require (
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
 	github.com/stretchr/testify v1.8.2 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/sys v0.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/git/client.go
+++ internal/git/client.go
@@ -3,9 +3,12 @@ package git
 import (
 	"context"
 	"errors"
+	"time"
 
+	"github.com/dominikus1993/kup50-tfs/internal/datetime"
 	"github.com/microsoft/azure-devops-go-api/azuredevops"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/git"
+	log "github.com/sirupsen/logrus"
 )
 
 type AzureDevopsClient struct {
@@ -25,19 +28,27 @@ func NewAzureDevopsClient(ctx context.Context, organizationUrl, token, project s
 }
 
 func (client *AzureDevopsClient) GetCommits(ctx context.Context, author string) error {
+	firstDay, lastDay := datetime.FirstAndLastDayOfTheMonth(time.Now())
 	repositories, err := client.gitClient.GetRepositories(ctx, git.GetRepositoriesArgs{Project: &client.project})
 	if err != nil {
 		return err
 	}
 	for _, repo := range *repositories {
 		repoId := repo.Id.String()
-		commits, commitErr := client.gitClient.GetCommits(ctx, git.GetCommitsArgs{RepositoryId: &repoId, Project: &client.project, SearchCriteria: &git.GitQueryCommitsCriteria{Author: &author, FromDate: }})
+		commits, commitErr := client.gitClient.GetCommits(ctx, git.GetCommitsArgs{RepositoryId: &repoId, Project: &client.project, SearchCriteria: &git.GitQueryCommitsCriteria{Author: &author, FromDate: &firstDay, ToDate: &lastDay}})
 		if err != nil {
 			err = errors.Join(err, commitErr)
+			log.WithError(err).Warnln("can't download commits")
 			continue
 		}
 		for _, commit := range *commits {
-
+			changes, err := client.gitClient.GetChanges(ctx, git.GetChangesArgs{CommitId: commit.CommitId, RepositoryId: &repoId})
+			if err != nil {
+				err = errors.Join(err, commitErr)
+				log.WithError(err).Warnln("can't download changes")
+				continue
+			}
+			changes.Changes
 		}
 	}
 }
