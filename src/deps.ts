2374d3d2875c92bc683ccbed2e10e483e7944db9
Dominik.Kotecki <dominikus1910@gmail.com>
2023-03-11 15:01:42 +0100

    .

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,3 @@
 export { compress, } from "https://deno.land/x/zip@v1.2.5/mod.ts";
 export { format } from "https://deno.land/std@0.179.0/datetime/mod.ts";
+export { filter, from, mergeMap, toArray, Observable } from "rxjs";
