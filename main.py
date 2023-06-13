cd8030f753e795b38a74e1472a5266903e6a2f0b
Dominik.Kotecki <dominikus1910@gmail.com>
2023-06-13 19:01:55 +0200

    python

---
+++ main.py
@@ -0,0 +1,7 @@
+from azure.devops.connection import Connection
+from msrest.authentication import BasicAuthentication
+from azure.devops.v7_1.git.models import GitQueryCommitsCriteria
+
+connection = Connection()
+
+import pprint
