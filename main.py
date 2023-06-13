36246bbb85c25c61c1f96c11e31d7116c8a2604d
Dominik.Kotecki <dominikus1910@gmail.com>
2023-06-13 17:31:16 +0000

    fix

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
 from azure.devops.v7_1.git.models import GitQueryCommitsCriteria
 from azure.devops.v7_1.core.core_client import CoreClient
+from azure.devops.v7_1.core.models import TeamProjectReference
 import pprint
-
 # Fill in with your personal access token and org URL
 personal_access_token = 'YOURPAT'
 organization_url = 'https://dev.azure.com/YOURORG'
@@ -15,7 +15,8 @@ connection = Connection(base_url=organization_url, creds=credentials)
 core_client: CoreClient = connection.clients.get_core_client()
 
 # Get the first page of projects
-get_projects_response = core_client.get_projects()
+get_projects_response: TeamProjectReference = core_client.get_projects()
+
 index = 0
 while get_projects_response is not None:
     for project in get_projects_response.value:
