d320bf92eb649da79df228ae7560c2932225f5fd
Dominik.Kotecki <dominikus1910@gmail.com>
2023-06-13 17:26:16 +0000

    .

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
 from azure.devops.v7_1.git.models import GitQueryCommitsCriteria
 from azure.devops.v7_1.core.core_client import CoreClient
+import pprint
+
 # Fill in with your personal access token and org URL
 personal_access_token = 'YOURPAT'
 organization_url = 'https://dev.azure.com/YOURORG'
@@ -10,9 +12,18 @@ organization_url = 'https://dev.azure.com/YOURORG'
 credentials = BasicAuthentication('', personal_access_token)
 
 connection = Connection(base_url=organization_url, creds=credentials)
-connection.get_client()
 core_client: CoreClient = connection.clients.get_core_client()
 
 # Get the first page of projects
 get_projects_response = core_client.get_projects()
-import pprint
+index = 0
+while get_projects_response is not None:
+    for project in get_projects_response.value:
+        pprint.pprint("[" + str(index) + "] " + project.name)
+        index += 1
+    if get_projects_response.continuation_token is not None and get_projects_response.continuation_token != "":
+        # Get the next page of projects
+        get_projects_response = core_client.get_projects(continuation_token=get_projects_response.continuation_token)
+    else:
+        # All projects have been retrieved
+        get_projects_response = None
