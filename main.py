f64b885b50e2c5769b31996450ab7bdc789eb481
Dominik.Kotecki <dominikus1910@gmail.com>
2023-06-13 17:24:42 +0000

    .

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
 from azure.devops.v7_1.git.models import GitQueryCommitsCriteria
+from azure.devops.v7_1.core.core_client import CoreClient
+# Fill in with your personal access token and org URL
+personal_access_token = 'YOURPAT'
+organization_url = 'https://dev.azure.com/YOURORG'
 
-connection = Connection()
+# Create a connection to the org
+credentials = BasicAuthentication('', personal_access_token)
 
+connection = Connection(base_url=organization_url, creds=credentials)
+connection.get_client()
+core_client: CoreClient = connection.clients.get_core_client()
+
+# Get the first page of projects
+get_projects_response = core_client.get_projects()
 import pprint
