73ec63238bf5ad53eca9b9551e204d192c7679ed
Dominik.Kotecki <dominikus1910@gmail.com>
2023-06-17 17:39:09 +0000

    .

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
 from azure.devops.v7_1.git.models import GitQueryCommitsCriteria
 from azure.devops.v7_1.core.core_client import CoreClient
+from azure.devops.v7_1.git.git_client import GitClient
+from azure.devops.v7_1.git.models import GitRepository
 from azure.devops.v7_1.core.models import TeamProjectReference
 import pprint
 import os
@@ -14,13 +16,17 @@ credentials = BasicAuthentication('', personal_access_token)
 
 connection = Connection(base_url=organization_url, creds=credentials)
 core_client: CoreClient = connection.clients.get_core_client()
-
+git_client: GitClient = connection.clients.get_git_client()
+git_client.get_repositories()
 # Get the first page of projects
 get_projects_response: list[TeamProjectReference] | None = core_client.get_projects()
 
 index = 0
 while get_projects_response is not None and len(get_projects_response) > 0:
     for project in get_projects_response :
+        project_id = project.id
+        repositories: list[GitRepository] = git_client.get_repositories(project=project_id)
+        pprint.pprint("xD" + str(len(repositories)))
         pprint.pprint("[" + str(index) + "] " + project.name)
         index += 1
     get_projects_response = core_client.get_projects(continuation_token=index)
