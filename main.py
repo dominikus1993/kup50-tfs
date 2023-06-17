02ffdbadf4d7b450d47f206cc06c251b08645807
Dominik.Kotecki <dominikus1910@gmail.com>
2023-06-17 15:34:26 +0000

    .

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ connection = Connection(base_url=organization_url, creds=credentials)
 core_client: CoreClient = connection.clients.get_core_client()
 
 # Get the first page of projects
-get_projects_response: TeamProjectReference = core_client.get_projects()
+get_projects_response: list[TeamProjectReference] | None = core_client.get_projects()
 
 index = 0
 while get_projects_response is not None:
-    for project in get_projects_response.value:
+    for project in get_projects_response.value :
         pprint.pprint("[" + str(index) + "] " + project.name)
         index += 1
     if get_projects_response.continuation_token is not None and get_projects_response.continuation_token != "":
